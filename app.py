62c2d2b26aeb08a74ad058502a8c2f7eb252d8e7
root <root@ip-172-31-92-215.ec2.internal>
2024-08-04 07:08:50 +0000

    added

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
-#app = Flask(__name__)
 app = Flask(__name__, static_url_path='/static')
+
 # MySQL Connection
 db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="Redhat@123",
     database="myproject"
-        )
-
+)
 
 cursor = db.cursor()
 
@@ -26,36 +25,38 @@ def login():
     cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
     user = cursor.fetchone()
     if user:
-         #return "Login successful!"
-        return redirect("https://facebook.com")
+        return redirect(url_for('videos'))
     else:
+        return "Invalid username or password"
 
-         return "Invalid username or password"
-    # Login logic here
+@app.route('/documents')
+def documents():
+    return render_template('documents.html')
+
+@app.route('/videos')
+def videos():
+    return render_template('videos.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-       try:
-        # Handle registration logic here
-        # For example, you can fetch form data and insert it into the database
-        username = request.form['username']
-        password = request.form['password']
+        try:
+            username = request.form['username']
+            password = request.form['password']
 
-        cursor.execute("SELECT * FROM users WHERE username=%s", (username,))
-        existing_user = cursor.fetchone()
-        if existing_user:
-          return "Username already exists"
-        else:
-          cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
-          db.commit()
-        # Insert logic here
-          return "Registration successful!"  # Return a valid responsei
-       except Exception as e:
-        db.rollback()
-        return f"Error occurred: {str(e)}"
+            cursor.execute("SELECT * FROM users WHERE username=%s", (username,))
+            existing_user = cursor.fetchone()
+            if existing_user:
+                return "Username already exists"
+            else:
+                cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+                db.commit()
+                return "Registration successful!"
+        except Exception as e:
+            db.rollback()
+            return f"Error occurred: {str(e)}"
     else:
-          return render_template('register.html')
+        return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
